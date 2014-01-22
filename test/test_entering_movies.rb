54b719eb84368bcdf64082a7237333eb4d5f9639
Drew Blumberg <alblumberg21@gmail.com>
2014-01-21 23:59:28 -0600

    Input validation tests added

--- test/test_entering_movies.rb
+++ test/test_entering_movies.rb
@@ -6,4 +6,14 @@ class TestEnteringMovies < MiniTest::Unit::TestCase
     expected = "Theoretically creating: a movie titled Anchorman2 with genre Comedy, year 2013, length 100, budget 20000000, and mpaa R"
     assert_command_output expected, command
   end
+
+  def test_valid_movie_gets_saved
+    skip "needs implementation"
+    assert false, "Missing test implementation"
+  end
+
+  def test_invalid_movie_doesnt_get_saved
+    skip "needs implementation"
+    assert false, "Missing test implementation"
+  end
 end
