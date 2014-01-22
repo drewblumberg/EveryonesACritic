f94d0e3c0cd99819536260e294b5b20087fe9466
Drew Blumberg <alblumberg21@gmail.com>
2014-01-21 23:58:03 -0600

    Option parsing for eac, passing test for movie creation

--- test/test_entering_movies.rb
+++ test/test_entering_movies.rb
@@ -2,7 +2,7 @@ require_relative 'helper'
 
 class TestEnteringMovies < MiniTest::Unit::TestCase
   def test_valid_movie_information_gets_printed
-    command = "./eac create movie --title Anchorman2 --genre 4 --year 2013 --length 100 --budget 20000000 --mpaa R"
+    command = "./eac create movie Anchorman2 --genre Comedy --year 2013 --length 100 --budget 20000000 --mpaa R"
     expected = "Theoretically creating: a movie titled Anchorman2 with genre Comedy, year 2013, length 100, budget 20000000, and mpaa R"
     assert_command_output expected, command
   end
