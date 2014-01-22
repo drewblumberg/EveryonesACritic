9979bc83d73a59a4aa6925ebcda1ffef05eb604d
Drew Blumberg <alblumberg21@gmail.com>
2014-01-22 15:01:35 -0600

    Movie successfully saving to db

--- test/test_entering_movies.rb
+++ test/test_entering_movies.rb
@@ -3,9 +3,13 @@ require 'sqlite3'
 
 class TestEnteringMovies < MiniTest::Unit::TestCase
   def test_valid_movie_information_gets_printed
-    command = "./eac create movie Anchorman2 --genre Comedy --year 2013 --length 100 --budget 20000000 --mpaa R"
-    expected = "Theoretically creating: a movie titled Anchorman2 with genre Comedy, year 2013, length 100, budget 20000000, and mpaa R"
+    command = "./eac create movie Anchorman2 --genre 4 --year 2013 --length 100 --budget 20000000 --mpaa R --environment test"
+    expected = "A movie titled Anchorman2 with genre 4, year 2013, length 100, budget 20000000, and mpaa R was created."
     assert_command_output expected, command
+
+    # Delete movie from db
+    db = SQLite3::Database.new "./db/critic_test.db"
+    db.execute "DELETE FROM movies WHERE title = 'Anchorman2'"
   end
 
   def test_valid_movie_gets_saved
