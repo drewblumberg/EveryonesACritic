476c342ac914eb1e0c0d61a65d200257c9a9d8f1
Drew Blumberg <alblumberg21@gmail.com>
2014-01-28 10:56:03 -0600

    Fixed typo in test and added test for no movie found in search

--- test/test_searching_movies.rb
+++ test/test_searching_movies.rb
@@ -2,7 +2,14 @@ require_relative 'helper'
 
 class TestSearchingMovies < MovieTest
   def test_search_returns_movie
-    command = './eac search "Matrix The" --environment test'
+    command = './eac search movie "Matrix The" --environment test'
     expected = 'Matrix The (8.5 review score): a 1 released in 1999, it has a length of 136 minutes, a budget of $63000000, and is rated R.'
+    assert_command_output expected, command
+  end
+
+  def test_search_returns_no_movie
+    command = './eac search movie "asdfghjasdfghj" --environment test'
+    expected = "No movie found with title 'asdfghjasdfghj'. Would you like to create one? Just use the command 'movie create'."
+    assert_command_output expected, command
   end
 end
